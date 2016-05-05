ff0e073ca2e2b4612d32c80196ae301e07775095
FlyRoNik <hang.glider.viru007@gmail.com>
2016-05-05 12:10:22 +0300

    Начал реализовывать history

--- src/ua/com/juja/sqlcmd/controller/MainController.java
+++ src/ua/com/juja/sqlcmd/controller/MainController.java
@@ -4,6 +4,9 @@ import ua.com.juja.sqlcmd.controller.command.*;
 import ua.com.juja.sqlcmd.model.DatabaseManager;
 import ua.com.juja.sqlcmd.view.View;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by FlyRoNik on 19.04.2016.
  */
@@ -11,6 +14,7 @@ public class MainController {
 
     private Command[] commands;
     private View view;
+    private List<String> history = new LinkedList<>();
 
     public MainController(View view, DatabaseManager manager) {
         this.view = view;
@@ -45,6 +49,7 @@ public class MainController {
                 try {
                     if (command.canProcess(input)) {
                         command.process(input);
+                        history.add(input);
                         break;
                     }
                 } catch (Exception e) {
